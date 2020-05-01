2edb8f0155f19772675d403d51d7b619d53841d6
Andrew Aiello <andrew@Andrews-MBP.modem>
2020-05-01 12:38:19 +1000

    finished creating a geochart

--- src/components/GeoChart/GeoChart.js
+++ src/components/GeoChart/GeoChart.js
@@ -3,17 +3,17 @@ import { select, geoPath, geoMercator, min, max, scaleLinear } from 'd3'
 import { useResizeObserver } from '../../hooks/useResizeObserver'
 
 const GeoChart = ({ data, property, setProperty }) => {
-    const svgRef = useRef()
+    const geoChartRef = useRef()
     const wrapperRef = useRef()
     const dimensions = useResizeObserver(wrapperRef)
     const [selectedCountry, setSelectedCountry] = useState(null)
 
     useEffect(() => {
-        const svg = select(svgRef.current)
+        const svg = select(geoChartRef.current)
 
         // use resixed dimensions
         // but fallback on getBoundingClientRect if there are no dimensions yet
-        const { width, height } = dimensions || wrapperRef.current.getBoundingClientRect
+        const { width, height } = dimensions || wrapperRef.current.getBoundingClientRect()
 
         const minProp = min(data.features, feature => feature.properties[property])
         const maxProp = max(data.features, feature => feature.properties[property])
@@ -22,8 +22,9 @@ const GeoChart = ({ data, property, setProperty }) => {
             .range(["#ccc", "red"])
 
         // Projects coordinates on a 2D plane
-        const projection = geoMercator().fitSize([width, height], 
-            selectedCountry || data)
+        const projection = geoMercator()
+            .fitSize([width, height], selectedCountry || data)
+            .precision(100)
 
         // Takes geojson data,
         // Transforms that into the d attribute of a path element
@@ -31,6 +32,7 @@ const GeoChart = ({ data, property, setProperty }) => {
 
         console.log(data.features)
 
+        // Render each country
         svg
             .selectAll(".country")
             .data(data.features)
@@ -40,8 +42,23 @@ const GeoChart = ({ data, property, setProperty }) => {
                 setSelectedCountry(selectedCountry === feature ? null : feature)
             )
             .transition()
+            .duration(1000)
             .attr("fill", feature => colorScale(feature.properties[property]))
             .attr("d", feature => pathGenerator(feature))
+
+        
+        // Render text
+        svg
+            .selectAll(".label")
+            .data([selectedCountry])
+            .join("text")
+            .attr("class", "label")
+            .text(
+                feature => feature &&
+                `${feature.properties.name}: ${feature.properties[property].toLocaleString()}`
+            )
+            .attr("x", 10)
+            .attr("y", 25)
 
     }, [data, dimensions, property, selectedCountry])
 
@@ -49,7 +66,7 @@ const GeoChart = ({ data, property, setProperty }) => {
 
     return (
         <div ref={wrapperRef} className="geoChart">
-            <svg ref={svgRef}></svg>
+            <svg ref={geoChartRef}></svg>
         </div>
     )
 }
