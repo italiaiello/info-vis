f5b881d8591688bfbe7d706991b09ea72dc529fd
Andrew Aiello <andrewaiello7@gmail.com>
2020-05-24 12:53:00 +1000

    Updates

---
+++ precache-manifest.f19a556fa6703193bc4cf80a10deffb4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b8d1f3689074a7c2e70b219805cc94",
+    "url": "/info-vis/index.html"
+  },
+  {
+    "revision": "20751fd2520c1a53417a",
+    "url": "/info-vis/static/css/main.06f6bb61.chunk.css"
+  },
+  {
+    "revision": "12d9df455a08ea4ca3e4",
+    "url": "/info-vis/static/js/2.4c4fb087.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/info-vis/static/js/2.4c4fb087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20751fd2520c1a53417a",
+    "url": "/info-vis/static/js/main.be4dc770.chunk.js"
+  },
+  {
+    "revision": "d7016881d780d8ad0de9",
+    "url": "/info-vis/static/js/runtime-main.e1e3a3f8.js"
+  },
+  {
+    "revision": "1488bc2d1cea5d36043bb9b983461852",
+    "url": "/info-vis/static/media/closeOpenSpaces.1488bc2d.csv"
+  },
+  {
+    "revision": "11227ee28622af559120ac7ddb0cd82d",
+    "url": "/info-vis/static/media/culpritDemographics.11227ee2.csv"
+  },
+  {
+    "revision": "773507f0746991fd632628845ef295ee",
+    "url": "/info-vis/static/media/mass_shootings.773507f0.csv"
+  },
+  {
+    "revision": "ef2a8fae36a32f013e606de1fd4ed6a5",
+    "url": "/info-vis/static/media/militaryShooters.ef2a8fae.csv"
+  },
+  {
+    "revision": "ad365587e749356b7398b364eba4c8ec",
+    "url": "/info-vis/static/media/numMassShootings.ad365587.csv"
+  },
+  {
+    "revision": "005a85c25f51c2c9bc4da75851693207",
+    "url": "/info-vis/static/media/targetsFrequency.005a85c2.csv"
+  },
+  {
+    "revision": "1b622d14b969f081cc4f2cb99ee80ad4",
+    "url": "/info-vis/static/media/victimsPerState.1b622d14.csv"
+  }
+]);
